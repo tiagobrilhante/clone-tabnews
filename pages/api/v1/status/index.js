0edd0cc6dbeb18c68b2f6f729e96f4b2d79acb92
tiagobrilhante <tiagobrilhantemania@gmail.com>
2024-11-24 07:53:46 -0400

    ajustes e criação do módulo `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function index(request, response) {
+import database from "../../../../infra/database";
+
+async function index(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor são teste açaí" });
 }
 
