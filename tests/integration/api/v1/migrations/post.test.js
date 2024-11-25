c94ed42f3c24bc0293dd8f32cdee05a99e23033c
tiagobrilhante <tiagobrilhantemania@gmail.com>
2024-11-25 15:39:21 -0400

    Ajustes de testes e melhorias de migration

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -10,7 +10,16 @@ test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(201);
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toEqual(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(200);
+  const response1Body = await response1.json();
+  expect(Array.isArray(response1Body)).toEqual(true);
+  expect(response1Body.length).toBe(0);
 });
