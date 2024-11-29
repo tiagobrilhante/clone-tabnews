b1cf9e3ca92ba33c3883a8c87e86682f44c48ff6
tiagobrilhante <tiagobrilhantemania@gmail.com>
2024-11-29 08:31:13 -0400

    chore(commitlint.config.js): Enhance commitlint config by ignoring body line length
    This update to the commitlint configuration file includes a rule to ignore the body line length, allowing for more flexible commit message formatting. Additionally, it maintains other existing rules to support clearer and more descriptive commits.

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,6 +20,7 @@ module.exports = {
     ],
     "header-max-length": [2, "always", 300], // Define limite maior, como 300 caracteres
     "body-max-length": [2, "always", 300], // Define limite maior, como 300 caracteres
+    "body-max-line-length": [0],
     "body-empty": [0], // Define limite maior, como 300 caracteres
     "subject-full-stop": [0], // Permite o título sem ponto final
     "subject-case": [0], // Permite qualquer caixa no título
