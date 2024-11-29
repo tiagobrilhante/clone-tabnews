25b270e37f10a57e88c6c7f0ae17bf285b0d3521
tiagobrilhante <tiagobrilhantemania@gmail.com>
2024-11-29 07:29:05 -0400

    feat(commitlint.config.js): Add more types to commitlint configuration
    Extended the type-enum list in commitlint.config.js to include "docs", "style", and "test". This allows for more granular commit categorization and improves commit message consistency.

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,9 +4,22 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["chore", "feat", "fix", "refactor", "ci", "revert"],
+      [
+        "chore",
+        "feat",
+        "fix",
+        "refactor",
+        "ci",
+        "revert",
+        "docs",
+        "style",
+        "test",
+        "build",
+        "perf",
+      ],
     ],
-    "header-max-length": [2, "always", 100], // Define limite maior, como 100 caracteres
+    "header-max-length": [2, "always", 300], // Define limite maior, como 300 caracteres
+    "body-max-line-length": [2, "always", 300], // Define limite maior, como 300 caracteres
     "subject-full-stop": [0], // Permite o título sem ponto final
     "subject-case": [0], // Permite qualquer caixa no título
   },
