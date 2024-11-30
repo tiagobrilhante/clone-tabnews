917d061f254659b08fa4cdbe699f546a6b870fd7
tiagobrilhante <tiagobrilhantemania@gmail.com>
2024-11-30 17:18:59 -0400

    fix: fixed a small bug

--- pages/status/index.js
+++ pages/status/index.js
@@ -47,12 +47,12 @@ function DatabaseStatus() {
         </div>
       </>
     );
-
-    return (
-      <>
-        <h2>Database</h2>
-        <div>{databaseStatusInformation}</div>
-      </>
-    );
   }
+
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
 }
