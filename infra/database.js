e8f0a2a39313d44052c483f063e2e9b933b9b3b9
tiagobrilhante <tiagobrilhantemania@gmail.com>
2025-02-08 10:03:58 -0400

    feat: refactor error handling by moving handlers to a new controller module and enhancing service error management

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -6,9 +7,11 @@ async function query(queryObject) {
     client = await getNewClient();
     return await client.query(queryObject);
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js: ");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,34 +1,12 @@
 import { createRouter } from "next-connect";
 import database from "infra/database";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import controller from "infra/controller.js";
 
 const router = createRouter();
 
 router.get(getHandler);
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
 
-function onNoMatchHandler(request, response) {
-  /*response.status(405).json({
-    error: `Method "${request.method}" not allowed`,
-  });*/
-  const publicErrorObject = new MethodNotAllowedError();
-
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-  console.log("\n Erro dentro do catch do next-connect: ");
-  console.error(publicErrorObject);
-
-  //response.status(500).json({ error: "Internal Server Error" });
-  response.status(500).json(publicErrorObject);
-}
+export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const updatedAt = new Date().toISOString();
