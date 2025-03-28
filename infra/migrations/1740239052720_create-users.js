0393ea308e2eb6bf12389fed5a8572497237ea78
tiagobrilhante <tiagobrilhantemania@gmail.com>
2025-03-28 15:01:00 -0400

    feat: update user table schema to improve password length and set UTC timezone for timestamps

--- infra/migrations/1740239052720_create-users.js
+++ infra/migrations/1740239052720_create-users.js
@@ -7,16 +7,16 @@ exports.up = (pgm) => {
     },
     username: { type: "varchar(30)", notNull: true, unique: true },
     email: { type: "varchar(254)", notNull: true, unique: true },
-    password: { type: "varchar(72)", notNull: true },
+    password: { type: "varchar(60)", notNull: true },
     created_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc' ,now())"),
     },
     updated_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc' ,now())"),
     },
   });
 };
