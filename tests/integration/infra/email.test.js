cf8c1f6d09de882cf72be6e393014c42b2be953d
Tiago Brilhante <tiagobrilhantemania@gmail.com>
2025-09-11 18:55:59 -0400

    feat: enhance email integration testing and orchestrator functionality
    - Added `orchestrator.waitForAllServices` to synchronize service startup for tests.
    - Enhanced email test with validation for the last sent email's content.
    - Introduced `deleteAllEmails` and `getLastEmail` methods in orchestrator.
    - Added MailCatcher HTTP configurations to `.env.development`.

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,12 +1,33 @@
 import email from "infra/email";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 
 describe("infra/email.js", () => {
   test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+
     await email.send({
       from: "Teste <teste@teste.com.br>",
       to: "StdOut <contato@stdout.deb.br>",
       subject: "Teste de Assunto",
       text: "Teste de Corpo",
     });
+
+    await email.send({
+      from: "Teste <teste@teste.com.br>",
+      to: "StdOut <contato@stdout.deb.br>",
+      subject: "Último email enviado",
+      text: "Corpo do Último Email",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+
+    expect(lastEmail.sender).toBe("<teste@teste.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<contato@stdout.deb.br>");
+    expect(lastEmail.subject).toBe("Último email enviado");
+    expect(lastEmail.text).toBe("Corpo do Último Email\r\n");
   });
 });
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,8 +5,11 @@ import migrator from "models/migrator.js";
 import user from "models/user.js";
 import session from "models/session.js";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -23,6 +26,30 @@ async function waitForAllServices() {
       // eslint-disable-next-line no-useless-catch
       try {
         const response = await fetch("http://localhost:3000/api/v1/status");
+        if (response.status !== 200) {
+          throw new Error(`Unexpected status code: ${response.status}`);
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+      onRetry: (error, attempt) => {
+        console.log(
+          `Attempt ${attempt} failed to fetch email page: ${error.message}`,
+        );
+      },
+    });
+
+    async function fetchEmailPage() {
+      // eslint-disable-next-line no-useless-catch
+      try {
+        const response = await fetch(emailHttpUrl);
         if (response.status !== 200) {
           throw new Error(`Unexpected status code: ${response.status}`);
         }
@@ -59,11 +86,31 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, { method: "DELETE" });
+}
+
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+  const emailListBody = await emailListResponse.json();
+  const lastEmailItem = emailListBody.pop();
+  const emailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+
+  const emailTextBody = await emailTextResponse.text();
+  lastEmailItem.text = emailTextBody;
+
+  return lastEmailItem;
+}
+
 const orchestrator = {
   waitForAllServices,
   createUser,
   createSession,
   clearDatabase,
   runPendingMigrations,
+  deleteAllEmails,
+  getLastEmail,
 };
 export default orchestrator;
