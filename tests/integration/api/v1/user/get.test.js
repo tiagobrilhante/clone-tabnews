c2b0e325e0d9ebeca14cc1f3464abfeb84cd07c9
Tiago Brilhante <tiagobrilhantemania@gmail.com>
2026-01-13 10:59:32 -0400

    feat(users): add "update:user" feature, enhance permissions and tests
    - Added the "update:user" feature to activated users.
    - Updated permissions middleware to check "update:user" during user update requests.
    - Enhanced integration tests to validate the propagation of "update:user" in user features.
    - Added `@types/jest` and adjusted configurations for improved Jest compatibility.
    - Updated ESLint and Jest environment settings to include necessary types.

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -57,7 +57,7 @@ describe("GET /api/v1/user", () => {
         username: "UserWithValidSession",
         email: createdUser.email,
         password: createdUser.password,
-        features: ["create:session", "read:session"],
+        features: ["create:session", "read:session", "update:user"],
         created_at: createdUser.created_at.toISOString(),
         updated_at: activatedUser.updated_at.toISOString(),
       });
@@ -121,7 +121,7 @@ describe("GET /api/v1/user", () => {
         id: createdUser.id,
         username: "UserWithHalfwayExpiredSession",
         email: createdUser.email,
-        features: ["create:session", "read:session"],
+        features: ["create:session", "read:session", "update:user"],
         password: createdUser.password,
         created_at: createdUser.created_at.toISOString(),
         updated_at: activatedUser.updated_at.toISOString(),
