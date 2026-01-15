9261dbeeaebacedd014197cb083355ba9854b427
Tiago Brilhante <tiagobrilhantemania@gmail.com>
2026-01-15 14:10:53 -0400

    feat(users): enforce "update:user" permission checks and add related tests
    - Implemented authorization checks for the "update:user" feature in user update requests.
    - Added validation to ensure users can only update their own data or have sufficient permissions.
    - Updated integration tests to include scenarios for unauthorized user updates.
    - Enhanced `authorization.can` to support resource-based permission evaluation.

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,6 +1,8 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import user from "models/user.js";
+import authorization from "models/authorization.js";
+import { ForbiddenError } from "infra/errors.js";
 
 const router = createRouter();
 
@@ -22,6 +24,18 @@ async function patchHandler(request, response) {
   const username = request.query.username;
 
   const userInputValues = request.body;
+
+  // user, feature, resource
+  const userTryingToPatch = request.context.user;
+  const targetUser = await user.findOneByUsername(username);
+
+  if (!authorization.can(userTryingToPatch, "update:user", targetUser)) {
+    throw new ForbiddenError({
+      message: "Você não possui permissão para atualizar outro usuário.",
+      action:
+        "Verifique se você possui a feature necessária para atualizar outro usuário.",
+    });
+  }
 
   const updatedUser = await user.update(username, userInputValues);
 
